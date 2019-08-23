651e88f21e4c8ef838a49ed02e6caeac66c6e864
Matryoshika <jonathancarlsson159@gmail.com>
2019-08-23 17:49:01 +0200

    Update for 1.0.2

--- src/main/java/matryoshika/cachet/Cachet.java
+++ src/main/java/matryoshika/cachet/Cachet.java
@@ -18,7 +18,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class Cachet {
 	public static final String MODID = "cachet";
 	public static final String NAME = "Cachet";
-	public static final String VERSION = "1.0.1";
+	public static final String VERSION = "1.0.2";
 
 	private static Logger logger;
 	private static String NBT_TAG = "cachet:tick_timer";
