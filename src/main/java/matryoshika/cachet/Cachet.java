6b31ce9525c089697d682e9bba32d0dadc54a93a
Matryoshika <jonathancarlsson159@gmail.com>
2019-08-25 02:20:37 +0200

    Update to 1.0.3

--- src/main/java/matryoshika/cachet/Cachet.java
+++ src/main/java/matryoshika/cachet/Cachet.java
@@ -21,7 +21,7 @@ import net.minecraftforge.server.permission.PermissionAPI;
 public class Cachet {
 	public static final String MODID = "cachet";
 	public static final String NAME = "Cachet";
-	public static final String VERSION = "1.0.2";
+	public static final String VERSION = "1.0.3";
 
 	private static Logger logger;
 	private static String NBT_TAG = "cachet:tick_timer";
